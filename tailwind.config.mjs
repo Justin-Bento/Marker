c837f94ae464a6123aa28d3e2f33e255b15c4f18
Justin Bento <bento.a.justin@gmail.com>
2024-02-17 18:17:57 -0500

    Feature: Extend color theme in Tailwind config to include a chicago color scheme.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-        sans: ["Lexend Variable", "Inter", ...defaultTheme.fontFamily.sans],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lexend Variable', 'Inter', ...defaultTheme.fontFamily.sans]
       },
-			colors: {
-				'brand': {
-					'50': '#fef9ec',
-					'100': '#fbeeca',
-					'200': '#f8de8f',
-					'300': '#f4c757',
-					'400': '#f1b12e',
-					'500': '#ea9016',
-					'600': '#cf6c10',
-					'700': '#ac4c11',
-					'800': '#8c3b14',
-					'900': '#733114',
-					'950': '#421806',
-			},
-			
-			}
-		},
-	},
-	plugins: [],
-}
+      colors: {
+        brand: {
+          50: '#fef9ec',
+          100: '#fbeeca',
+          200: '#f8de8f',
+          300: '#f4c757',
+          400: '#f1b12e',
+          500: '#ea9016',
+          600: '#cf6c10',
+          700: '#ac4c11',
+          800: '#8c3b14',
+          900: '#733114',
+          950: '#421806'
+        },
+        chicago: {
+          50: '#f5f6f5',
+          100: '#e5e8e6',
+          200: '#ced3d1',
+          300: '#acb4b1',
+          400: '#828e8a',
+          500: '#67736f',
+          600: '#555e5b',
+          700: '#4b5351',
+          800: '#424847',
+          900: '#3a3f3e',
+          950: '#242827'
+        }
+      }
+    }
+  },
+  plugins: []
+};
