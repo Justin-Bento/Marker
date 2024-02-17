739b6657585ef3ee9517f3be9f3755319534d118
Justin Bento <bento.a.justin@gmail.com>
2024-02-17 18:12:19 -0500

    Feature: Extend color theme in Tailwind config to include a branded color scheme.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,22 @@ export default {
 			fontFamily: {
         sans: ["Lexend Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+			colors: {
+				'brand': {
+					'50': '#fef9ec',
+					'100': '#fbeeca',
+					'200': '#f8de8f',
+					'300': '#f4c757',
+					'400': '#f1b12e',
+					'500': '#ea9016',
+					'600': '#cf6c10',
+					'700': '#ac4c11',
+					'800': '#8c3b14',
+					'900': '#733114',
+					'950': '#421806',
+			},
+			
+			}
 		},
 	},
 	plugins: [],
